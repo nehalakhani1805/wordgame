41af2b642d0b2ba088cfbfdbb432595de1da5646
nehalakhani1805 <neha.lakhani1805@gmail.com>
2020-06-12 20:19:11 +0530

    first

---
+++ src/word_game.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author NEHA LAKHANI
+ */
+public class word_game {
+    public static void main(String[] args) {
+        
+        //Starting the first page.
+        first_page starting_program=new first_page(3);
+        starting_program.setVisible(true);
+        //Set the background.
+        starting_program.getContentPane().setBackground(new java.awt.Color(204,0,204));
+    }
+}
